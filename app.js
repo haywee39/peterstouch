cef10b5b6dccc451e8ed0e7da8675f6d1f24aef5
Your Name <[haywee39@gmail.com]>
2024-12-18 09:55:25 +0100

    redesign

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 
+// TYPEWRITE FOR THE COMPANY NAME 
+// const text = "THE PETERSTOUCH MERCHANTS"; 
+// const typingSpeed = 80; 
 
-const text = "THE PETERSTOUCH MERCHANTS"; // The text to display
-const typingSpeed = 80; // Typing speed in milliseconds
+// let i = 0;
 
-let i = 0;
+// function typeWriter() {
+//   const textElement = document.getElementById("text");
+//   if (i < text.length) {
+//     textElement.textContent += text.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, typingSpeed);
+//   }
+// }
 
-function typeWriter() {
-  const textElement = document.getElementById("text");
-  if (i < text.length) {
-    textElement.textContent += text.charAt(i);
-    i++;
-    setTimeout(typeWriter, typingSpeed);
-  }
-}
-
-window.onload = typeWriter;
+// window.onload = typeWriter;
 
 
 // ^^^^^^^^^^***************************************************************
+// TYPEWRITE FOR THE COMPANY SHORT DESC 
+const text1 = "THE PETERSTOUCH MERCHANTS";
+const text2 = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.ure eaque nisiProvident iusto fugiat culpa, quisquam iure eaque nisi aspernaturquisquam iure eaque nisi aspernaquisquam iure eaque nisi asperna"; 
+              
+const typingSpeed = 80;
+
+let i = 0;
+let j = 0;
+
+function typeWriter1() {
+  const textElement1 = document.getElementById("text");
+  if (i < text1.length) {
+    textElement1.textContent += text1.charAt(i);
+    i++;
+    setTimeout(typeWriter1, typingSpeed);
+  } else {
+    setTimeout(typeWriter2, 500); // Delay before starting the second typewriter
+  }
+}
+
+function typeWriter2() {
+  const textElement2 = document.getElementById("desc");
+  if (j < text2.length) {
+    textElement2.textContent += text2.charAt(j);
+    j++;
+    setTimeout(typeWriter2, typingSpeed);
+  }
+}
+
+window.onload = typeWriter1;
+
