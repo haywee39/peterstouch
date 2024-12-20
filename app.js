048609ff9de8f69741f5e6a5eec5cd005cb3939b
Your Name <[haywee39@gmail.com]>
2024-12-20 04:10:14 +0100

    bakslide

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-
-// TYPEWRITE FOR THE COMPANY NAME AND SHORT DESC 
+// Typewriter Effect
 const text1 = "THE PETERSTOUCH MERCHANTS";
-const text2 = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.ure eaque nisiProvident iusto fugiat culpa, quisquam iure eaque aspernaquisquam iure eaque nisi asperna"; 
-              
-const typingSpeed = 75;
+const text2 = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident iusto fugiat culpa, quisquam iure eaque nisi asperna";
 
+const typingSpeed = 75;
 let i = 0;
 let j = 0;
 
@@ -28,7 +26,62 @@ function typeWriter2() {
   }
 }
 
-window.onload = typeWriter1;
+// Background Image Rotator
+const backgroundImages = [
+  "./backgrdimg/backgrd.jpg",
+  "./backgrdimg/back1.jpg",
+  "./backgrdimg/mic-conf.jpg",
+  "./backgrdimg/conf-seat.jpg",
+  // "./backgrdimg/back4.jpg"
+];
+
+let currentImageIndex = 0;
+
+function rotateBackground() {
+  const homeSection = document.getElementById("home");
+  homeSection.style.backgroundImage = `url('${backgroundImages[currentImageIndex]}')`;
+  currentImageIndex = (currentImageIndex + 1) % backgroundImages.length; // Loop through images
+}
+
+window.onload = () => {
+  typeWriter1();
+  rotateBackground();
+  setInterval(rotateBackground, 5000); // Change background every 5 seconds
+};
+
+// ************************************************************************
+
+
+// TYPEWRITE FOR THE COMPANY NAME AND SHORT DESC 
+// const text1 = "THE PETERSTOUCH MERCHANTS";
+// const text2 = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.ure eaque nisiProvident iusto fugiat culpa, quisquam iure eaque aspernaquisquam iure eaque nisi asperna"; 
+              
+// const typingSpeed = 75;
+
+// let i = 0;
+// let j = 0;
+
+// function typeWriter1() {
+//   const textElement1 = document.getElementById("text");
+//   if (i < text1.length) {
+//     textElement1.textContent += text1.charAt(i);
+//     i++;
+//     setTimeout(typeWriter1, typingSpeed);
+//   } else {
+//     setTimeout(typeWriter2, 450); // Delay before starting the second typewriter
+//   }
+// }
+
+// function typeWriter2() {
+//   const textElement2 = document.getElementById("desc");
+//   if (j < text2.length) {
+//     textElement2.textContent += text2.charAt(j);
+//     j++;
+//     setTimeout(typeWriter2, typingSpeed);
+//   }
+// }
+
+// window.onload = typeWriter1;
 // ***************************************************************
 
 // COMPANY LOGO APPEARS ON Slide in TO THE POINT 
