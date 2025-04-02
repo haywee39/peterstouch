4f6304ce7c82265b5cfe9969ab28f2871b0d64d5
Your Name <[haywee39@gmail.com]>
2025-04-02 19:52:31 +0100

    real content

--- app.js
+++ app.js
@@ -28,10 +28,13 @@ function typeWriter2() {
 
 // Background Image Rotator
 const backgroundImages = [
+  "./backgrdimg/client2.jpg",
   "./backgrdimg/backgrd.jpg",
-  "./backgrdimg/back1.jpg",
-  "./backgrdimg/mic-conf.jpg",
-  "./backgrdimg/conf-seat.jpg"  
+  "./backgrdimg/ptrninstlr.jpg",
+  "./backgrdimg/conf-seat.jpg",
+  "./backgrdimg/scrnguyyelow.jpg",
+  "./backgrdimg/mic-conf.jpg"
+  
 ];
 
 let currentImageIndex = 0;
@@ -50,37 +53,6 @@ window.onload = () => {
 
 // ************************************************************************
 
-
-// TYPEWRITE FOR THE COMPANY NAME AND SHORT DESC 
-// const text1 = "THE PETERSTOUCH MERCHANTS";
-// const text2 = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.ure eaque nisiProvident iusto fugiat culpa, quisquam iure eaque aspernaquisquam iure eaque nisi asperna"; 
-              
-// const typingSpeed = 75;
-
-// let i = 0;
-// let j = 0;
-
-// function typeWriter1() {
-//   const textElement1 = document.getElementById("text");
-//   if (i < text1.length) {
-//     textElement1.textContent += text1.charAt(i);
-//     i++;
-//     setTimeout(typeWriter1, typingSpeed);
-//   } else {
-//     setTimeout(typeWriter2, 450); // Delay before starting the second typewriter
-//   }
-// }
-
-// function typeWriter2() {
-//   const textElement2 = document.getElementById("desc");
-//   if (j < text2.length) {
-//     textElement2.textContent += text2.charAt(j);
-//     j++;
-//     setTimeout(typeWriter2, typingSpeed);
-//   }
-// }
-
-// window.onload = typeWriter1;
 // ***************************************************************
 
 // COMPANY LOGO APPEARS ON Slide in TO THE POINT 
